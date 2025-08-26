6ab1d57bc0d8efc7621dea47ed6d79f95da68a13
Ajay Patidar <ajaypatidar.it@gmail.com>
2025-08-26 17:50:13 +0530

    fixed : Expense calculation

--- app/src/main/java/com/example/truesplit/utils/ExpenseUtils.kt
+++ app/src/main/java/com/example/truesplit/utils/ExpenseUtils.kt
@@ -28,11 +28,17 @@ object ExpenseUtils {
                 }
             } else {
                 if (paidBy != null && balances.containsKey(paidBy)) {
-                    val share = amount / members.size
-                    balances[paidBy] = balances.getValue(paidBy) + (amount - share)
-                    members.forEach { member ->
-                        if (member["id"] != paidBy) {
-                            balances[member["id"]!!] = balances.getValue(member["id"]!!) - share
+                    // Get the splits from the transaction
+                    val splits = transaction["splits"] as? Map<String, Any> ?: emptyMap()
+
+                    // The payer pays the full amount
+                    balances[paidBy] = balances.getValue(paidBy) + amount
+
+                    // Each member pays their share according to the splits
+                    splits.forEach { (memberId, shareAmount) ->
+                        val share = (shareAmount as? Number)?.toDouble() ?: 0.0
+                        if (balances.containsKey(memberId)) {
+                            balances[memberId] = balances.getValue(memberId) - share
                         }
                     }
                 }
